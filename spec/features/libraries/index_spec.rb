1af47299df13c3925d6f38b9db5ae2180fa0458d
Aliya Merali <merali4@gmail.com>
2021-05-12 11:49:42 -0600

    Add missing visit statement in test

--- spec/features/libraries/index_spec.rb
+++ spec/features/libraries/index_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'libraries index page', type: :feature do
     library1 = Library.create!(name: "Denver Central", staff_count: 35, member_count: 5000, open: true)
     library2 = Library.create!(name: "Ford-Warren", staff_count: 12, member_count: 1200, open: false)
     library3 = Library.create!(name: "Park Hill", staff_count: 20, member_count: 2300, open: true)
+    visit "/libraries"
 
     expect(page).to have_content(library1.name)
     expect(page).to have_content(library2.name)
