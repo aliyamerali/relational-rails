286c99e183905e7cafd87859a4f4f9ca1e27353c
Aliya Merali <merali4@gmail.com>
2021-05-13 16:42:47 -0600

    Fix test typos, add form to new view

--- spec/features/libraries/new_spec.rb
+++ spec/features/libraries/new_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe 'libraries new page', type: :feature do
 
   it 'has a form to create a new library' do
-    expect(page).to have_field("Library Name")
-    expect(page).to have_field("Library Staff Count")
-    expect(page).to have_field("Library Member Count")
-    expect(page).to have_field("Is this library open?")
+    visit '/libraries/new'
+    expect(page).to have_field('name')
+    expect(page).to have_field('staff_count')
+    expect(page).to have_field('member_count')
+    expect(page).to have_field('open')
   end
 
 end
