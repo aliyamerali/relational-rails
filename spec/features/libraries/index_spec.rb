0e4998a58b0b2b131c281c707e8b7f7558286388
Aliya Merali <merali4@gmail.com>
2021-05-13 14:15:04 -0600

    Fix test errors and add created_at timestamp to test data

--- spec/features/libraries/index_spec.rb
+++ spec/features/libraries/index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'libraries index page', type: :feature do
   before :each do
-    @library1 = Library.create!(name: "Denver Central", staff_count: 35, member_count: 5000, open: true)
-    @library2 = Library.create!(name: "Ford-Warren", staff_count: 12, member_count: 1200, open: false)
-    @library3 = Library.create!(name: "Park Hill", staff_count: 20, member_count: 2300, open: true)
+    @library1 = Library.create!(name: "Denver Central", staff_count: 35, member_count: 5000, open: true, created_at: DateTime.new(2019,2,3,4,5,6))
+    @library2 = Library.create!(name: "Ford-Warren", staff_count: 12, member_count: 1200, open: false, created_at: DateTime.new(2020,2,3,4,5,6))
+    @library3 = Library.create!(name: "Park Hill", staff_count: 20, member_count: 2300, open: true, created_at: DateTime.new(2021,2,3,4,5,6))
 
     visit "/libraries"
   end
@@ -17,8 +17,9 @@ RSpec.describe 'libraries index page', type: :feature do
   end
 
   it 'returns libraries in order of creation, with most recently created first' do
-    expect(@library1).to appear_before(@library2)
-    expect(@library2).to appear_before(@library3)
+    save_and_open_page
+    expect(@library3.name).to appear_before(@library2.name)
+    expect(@library2.name).to appear_before(@library1.name)
   end
 
   it 'shows when each record was created' do
