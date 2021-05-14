4b24718ca356afa08751203d787ca10345c3110f
Aliya Merali <merali4@gmail.com>
2021-05-14 15:26:22 -0600

    Cleanup tests and add link template in view

--- spec/features/libraries/books/index_spec.rb
+++ spec/features/libraries/books/index_spec.rb
@@ -1,62 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe 'Library books index' do
+  before :each do
+    @library1 = Library.create!(name: "Denver Central", staff_count: 35, member_count: 5000, open: true)
+    @library2 = Library.create!(name: "Ford-Warren", staff_count: 12, member_count: 1200, open: false)
+    @library3 = Library.create!(name: "Park Hill", staff_count: 20, member_count: 2300, open: true)
+
+    @book1 = @library1.books.create!(name: "Harry Potter and the Sorcerer's Stone", publish_year: 1998, available: true)
+    @book2 = @library1.books.create!(name: "1Q84", publish_year: 2011, available: false)
+
+    @book3 = @library2.books.create!(name: "On the Move: A Life", publish_year: 2015, available: true)
+    @book4 = @library2.books.create!(name: "The Vanishing Half", publish_year: 2020, available: false)
+
+    @book5 = @library3.books.create!(name: "I'm Just A Person", publish_year: 2016, available: true)
+    @book6 = @library3.books.create!(name: "The Ocean at the End of the Lane", publish_year: 2013, available: false)
+  end
 
   it 'shows all the books at a given library' do
-    library1 = Library.create!(name: "Denver Central", staff_count: 35, member_count: 5000, open: true)
-    library2 = Library.create!(name: "Ford-Warren", staff_count: 12, member_count: 1200, open: false)
-    library3 = Library.create!(name: "Park Hill", staff_count: 20, member_count: 2300, open: true)
+    visit "/libraries/#{@library1.id}/books"
+    expect(page).to have_content(@book1.name)
+    expect(page).to have_content(@book1.available)
+    expect(page).to have_content(@book2.name)
+    expect(page).to have_content(@book2.publish_year)
+    expect(page).not_to have_content(@book3.name)
 
-    book1 = library1.books.create!(name: "Harry Potter and the Sorcerer's Stone", publish_year: 1998, available: true)
-    book2 = library1.books.create!(name: "1Q84", publish_year: 2011, available: false)
-
-    book3 = library2.books.create!(name: "On the Move: A Life", publish_year: 2015, available: true)
-    book4 = library2.books.create!(name: "The Vanishing Half", publish_year: 2020, available: false)
-
-    book5 = library3.books.create!(name: "I'm Just A Person", publish_year: 2016, available: true)
-    book6 = library3.books.create!(name: "The Ocean at the End of the Lane", publish_year: 2013, available: false)
-
-    visit "/libraries/#{library1.id}/books"
-    expect(page).to have_content(book1.name)
-    expect(page).to have_content(book1.available)
-    expect(page).to have_content(book2.name)
-    expect(page).to have_content(book2.publish_year)
-    expect(page).not_to have_content(book3.name)
-
-    visit "/libraries/#{library2.id}/books"
-    expect(page).to have_content(book3.name)
-    expect(page).to have_content(book3.available)
-    expect(page).to have_content(book4.name)
-    expect(page).to have_content(book4.publish_year)
-    expect(page).not_to have_content(book6.name)
+    visit "/libraries/#{@library2.id}/books"
+    expect(page).to have_content(@book3.name)
+    expect(page).to have_content(@book3.available)
+    expect(page).to have_content(@book4.name)
+    expect(page).to have_content(@book4.publish_year)
+    expect(page).not_to have_content(@book6.name)
   end
 
   it 'has link to books and libraries indexes' do
-    library2 = Library.create!(name: "Ford-Warren", staff_count: 12, member_count: 1200, open: false)
-    visit "/libraries/#{library2.id}/books"
+    visit "/libraries/#{@library2.id}/books"
+
     expect(page).to have_link("Books Index", :href=>"/books")
     expect(page).to have_link("Libraries Index", :href=>"/libraries")
   end
 
   it 'has a link to create a new book' do
-    library2 = Library.create!(name: "Ford-Warren", staff_count: 12, member_count: 1200, open: false)
-    visit "/libraries/#{library2.id}/books"
+    visit "/libraries/#{@library2.id}/books"
 
-    expect(page).to have_link("Create Book", :href=>"/libraries/#{library2.id}/books/new")
+    expect(page).to have_link("Create Book", :href=>"/libraries/#{@library2.id}/books/new")
   end
 
   it 'has a link to sort alphabetically' do
-    library = Library.create!(name: "Ford-Warren", staff_count: 12, member_count: 1200, open: false)
-    book1 = library.books.create!(name: "Harry Potter and the Sorcerer's Stone", publish_year: 1998, available: true)
-    book2 = library.books.create!(name: "1Q84", publish_year: 2011, available: false)
+    visit "/libraries/#{@library1.id}/books"
 
-    visit "/libraries/#{library.id}/books"
-
-    expect(book1.name).to appear_before(book2.name)
-    expect(page).to have_link("Sort Books Alphabetically", :href=>"/libraries/#{library.id}/books/")
+    expect(@book1.name).to appear_before(@book2.name)
+    expect(page).to have_link("Sort Books Alphabetically", :href=>"/libraries/#{@library1.id}/books")
 
     click_link("Sort Books Alphabetically")
-    expect(book2.name).to appear_before(book1.name)
+    expect(@book2.name).to appear_before(@book1.name)
   end
 
 end
