b7fe7930d509cc63e88526f7c8dc6bb380fe2890
Aliya Merali <merali4@gmail.com>
2021-05-13 18:47:51 -0600

    Add test for form submission and redirect

--- spec/features/libraries/books/new_spec.rb
+++ spec/features/libraries/books/new_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe 'form to add a new book to a given library' do
     expect(page).to have_field('publish_year')
     expect(page).to have_field('available')
   end
+
+  it 'clicking submit redirects back to the library book page that shows the new book' do
+    @library = Library.create!(name: "Denver Central", staff_count: 35, member_count: 5000, open: true)
+    visit "/libraries/#{@library.id}/books/new"
+    fill_in 'name', with: 'Brave New Work'
+    fill_in 'publish_year', with: '2019'
+    choose('available', {option: true})
+    click_button 'submit'
+
+    expect(page).to have_current_path("/libraries/#{@library.id}/books")
+    expect(page).to have_content("Brave New Work")
+    expect(page).to have_content("Year Published: 2019")
+    expect(page).to have_content("Currently Available: true")
+  end
 end
