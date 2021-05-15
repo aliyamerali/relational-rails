10fba71b86904d5edde318ccfdab83e12b6c226d
Aliya Merali <merali4@gmail.com>
2021-05-15 17:28:36 -0600

    Add delete button to view, route, action in controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,4 +24,11 @@ class BooksController < ApplicationController
     redirect_to "/books/#{book.id}"
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+
+    redirect_to "/books"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get "/books/:id", to:'books#show'
   get "/books/:id/edit", to:'books#edit'
   patch "books/:id", to:'books#update'
+  delete "/books/:id", to:'books#destroy'
 
   # parent child
   get "/bartenders/:id/drinks", to: 'bartender_drinks#index'
