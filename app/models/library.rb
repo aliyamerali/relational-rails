048b159ddc49f15d89e42ed2f72e66e614382d65
Aliya Merali <merali4@gmail.com>
2021-05-17 15:25:25 -0600

    Add model methods to return book count and library sort by book count

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,3 +1,14 @@
 class Library < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  def book_count
+    self.books.count
+  end
+
+  def self.sort_by_book_count
+    libraries = Library.all
+    libraries.sort_by do |library|
+      library.book_count
+    end.reverse
+  end
 end
