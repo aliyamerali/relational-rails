d705c29059a289e7098ebcba6ab59417c09dce91
Joe Ray <jray100.jr@gmail.com>
2021-05-17 15:11:11 -0600

    Update controller with new and create action

--- app/controllers/bartenders_controller.rb
+++ app/controllers/bartenders_controller.rb
@@ -6,4 +6,19 @@ class BartendersController < ApplicationController
   def show
     @bartender = Bartender.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    bartender = Bartender.new(bartender_params)
+    
+    bartender.save
+    redirect_to '/bartenders'
+  end
+
+  private
+  def bartender_params
+    params.permit(:name, :age, :on_vacation, :city)
+  end
 end
