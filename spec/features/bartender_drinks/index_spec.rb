8d8c44d69895169ba81ef5bb0da6769abe962aa8
Joe Ray <jray100.jr@gmail.com>
2021-05-18 12:13:22 -0600

    Add features test for US-21, abv filter

--- spec/features/bartender_drinks/index_spec.rb
+++ spec/features/bartender_drinks/index_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe 'Bartender Drinks Index Page' do
 
     @drink1 = @bartender1.drinks.create!(name: "Martini", cost: 4.00, abv: 11, popular: true)
     @drink2 = @bartender1.drinks.create!(name: "4 horsmen", cost: 11.00, abv: 18, popular: false)
+    @drink7 = @bartender1.drinks.create!(name: "Manhatten", cost: 12.00, abv: 15, popular: true)
 
     @drink3 = @bartender2.drinks.create!(name: "Mojito", cost: 7.00, abv: 10, popular: true)
     @drink4 = @bartender2.drinks.create!(name: "Mai Tai", cost: 9.00, abv: 13, popular: true)
@@ -61,5 +62,27 @@ RSpec.describe 'Bartender Drinks Index Page' do
     end
 
     expect(current_path).to eq("/drinks/#{@drink1.id}/edit")
+  end
+
+  it "I see a form that allows me to input a number value and when I click submit only the records that meet that threshold are shown" do
+    expect(current_path).to eq("/bartenders/#{@bartender1.id}/drinks")
+    
+    expect(page).to have_field("filter")
+
+    fill_in 'filter', with: '13'
+    click_button "Only return records with more than number submitted of abv"
+
+    expect(page).to have_content(@drink2.name)
+    expect(page).to have_content(@drink2.cost)
+    expect(page).to have_content(@drink2.abv)
+    expect(page).to have_content(@drink2.popular)
+
+    expect(page).to have_content(@drink7.name)
+    expect(page).to have_content(@drink7.cost)
+    expect(page).to have_content(@drink7.abv)
+    expect(page).to have_content(@drink7.popular)
+
+    expect(page).to_not have_content(@drink1.name)
+    expect(page).to_not have_content(@drink1.cost)
   end
 end
