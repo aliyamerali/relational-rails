361ab4e78a2ddaf5b194bc524e0fb389ffd08557
Aliya Merali <merali4@gmail.com>
2021-05-18 14:01:32 -0600

    Update tests to move sort book by name function to be class method in book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,4 +4,18 @@ describe Book, type: :model do
   describe 'relationship' do
     it { should belong_to(:library) }
   end
+
+  describe 'class methods' do
+    it '#sort_by_name orders books by name' do
+      @library1 = Library.create!(name: "Denver Central", staff_count: 35, member_count: 5000, open: true, created_at: DateTime.new(2021,2,3,4,5,6))
+
+      @book1 = @library1.books.create!(name: "Harry Potter and the Sorcerer's Stone", publish_year: 1998, available: true)
+      @book2 = @library1.books.create!(name: "1Q84", publish_year: 2011, available: false)
+      @book3 = @library1.books.create!(name: "On the Move: A Life", publish_year: 2015, available: true)
+      books = @library1.books
+
+      expect(books.sort_by_name).to eq([@book2, @book1, @book3])
+    end
+
+  end
 end
